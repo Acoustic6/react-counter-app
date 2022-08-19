df6a01adc34a775d48f1d4ff5ccc296315a263b2
Acoustic66 <dorozhko.dmitry@gmail.com>
2022-08-20 00:05:43 +0300

    Updating the State

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: [],
   };
-
-  // constructor() {
-  //   super(); // this is not alowed before super()
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
 
   render() {
     return (
@@ -25,7 +19,19 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = () => console.log("Increment Clicked", this);
+  handleIncrement = () => {
+    //this.state.count++; // this wont work
+    // react is now aware that state's property was changed
+    // we need to use base React Component's method:
+    //this.setState();
+
+    // this way we notify React the state has changed
+    // react figures out what part of the state was changed and based on that it will sync DOM and virtual DOM
+    // in Angular all browser events are monkey patched and it detects changes automatuically
+    // in React we tell it explicitly what was changed
+
+    this.setState({ count: this.state.count + 1 }); // properties in this call will ovverride existed properties of current state.
+  };
 
   getBadgeClasses() {
     let classes = "badge m-2 ";
