71eced34c6238d23ce7e2c71d7ee2ee693f6441d
Acoustic66 <dorozhko.dmitry@gmail.com>
2022-08-23 22:53:22 +0300

    no signle source of truth problem

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -16,9 +16,18 @@ class Counters extends Component {
     this.setState({ counters });
   }
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({counters}); // won't work as we don't have a single source of truth
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
         {this.state.counters.map((counter) => (
           <Counter key={counter.id} onDelete={this.handleDelete} counter={counter}/>
         ))}
